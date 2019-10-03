e5f5cb92cb903368f841e1ea579af51a12b84acf
Nzubechukwu <nzubennamani@gmail.com>
2019-10-03 09:32:06 +0100

    setup a new env add language model add laguage serializer add lanuage view

---
+++ api_example/languages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Language
+from .serializer import LanguageSerializer
+
+# Create your views here.
+
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
